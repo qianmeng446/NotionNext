787eb27215f4a0120bb7bc7eaffeb8550a5c671b
qianmeng446 <qianmeng446@gmail.com>
2024-11-24 14:49:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,9 +25,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '千梦', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '热爱技术，喜欢交易，希望大家多多支持，点击下方可以联系我', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '千梦微信；dlsx41258,有任何问题可以加v联系，欢迎大家骚扰！', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://qianmeng666.us.kg', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '千梦, 空间站,节点', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '千梦,空间站,节点,qianmeng', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'qianmengsw@gmail.com', // 邮箱地址 例如mail@tangly1024.com
