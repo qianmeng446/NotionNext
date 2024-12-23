4cc75d7a47a8f98b079d97c94eda94fea278fb2d
qianmeng446 <qianmeng446@gmail.com>
2024-12-23 20:44:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,11 +27,11 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '千梦', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'qianmeng666.us.kg', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'qianmeng','千梦空间','千梦','千梦社区', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'qianmeng,千梦,千梦社区,千梦空间', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'qianmengCommunity@hotmail', // 邮箱地址 例如mail@tangly1024.com
-  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
+  CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || 'https://weibo.com/u/7284521123', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '@qianmeng446', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 't.me/qianmeng888', // 你的telegram 地址 例如 https://t.me/tangly_1024
